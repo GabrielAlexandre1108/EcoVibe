4d10fb37159c4aef6d1cb82f5efb28ded2593b28
GabrielAlexandre <gabriel.alexandre2938@gmail.com>
2025-01-22 11:12:26 -0300

    EcoVibe adicionada ao repositório

---
+++ README.md
@@ -0,0 +1,2 @@
+# EcoVibe
+ Este é um projeto de criação de site para ecoVibe, uma loja online dedicada a produtos sustentáveis e ecológicos.
---
+++ sobre/script/scrip.js
@@ -0,0 +1,5 @@
+/* window.addEventListener("scroll", function(){
+    let botao = document.querySelector('#botao')
+
+    botao.classList.toggle('aparecer',window.scrollY > 0)
+}) */
